7924d2bfa862bc199439a4fff6932c2ff20ed8a8
Marijose Rández <info@marijoserandez.com>
2014-04-29 10:35:05 +0200

    Creado plugins

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,14 +1,62 @@
 <?php
+/**
+ *
+ *
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Config
+ * @since         CakePHP(tm) v 0.2.9
+ */
 
+/**
+ * Database configuration class.
+ * You can specify multiple configurations for production, development and testing.
+ *
+ * datasource => The name of a supported datasource; valid options are as follows:
+ *  Database/Mysql - MySQL 4 & 5,
+ *  Database/Sqlite - SQLite (PHP5 only),
+ *  Database/Postgres - PostgreSQL 7 and higher,
+ *  Database/Sqlserver - Microsoft SQL Server 2005 and higher
+ *
+ * You can add custom database datasources (or override existing datasources) by adding the
+ * appropriate file to app/Model/Datasource/Database. Datasources should be named 'MyDatasource.php',
+ *
+ *
+ * persistent => true / false
+ * Determines whether or not the database should use a persistent connection
+ *
+ * host =>
+ * the host you connect to the database. To add a socket or port number, use 'port' => #
+ *
+ * prefix =>
+ * Uses the given prefix for all the tables in this database. This setting can be overridden
+ * on a per-table basis with the Model::$tablePrefix property.
+ *
+ * schema =>
+ * For Postgres/Sqlserver specifies which schema you would like to use the tables in. Postgres defaults to 'public'. For Sqlserver, it defaults to empty and use
+ * the connected user's default schema (typically 'dbo').
+ *
+ * encoding =>
+ * For MySQL, Postgres specifies the character encoding to use when connecting to the
+ * database. Uses database default not specified.
+ *
+ * unix_socket =>
+ * For MySQL to connect via socket specify the `unix_socket` parameter instead of `host` and `port`
+
+ * settings =>
+ * Array of key/value pairs, on connection it executes SET statements for each pair
+ * For MySQL : http://dev.mysql.com/doc/refman/5.6/en/set-statement.html
+ * For Postgres : http://www.postgresql.org/docs/9.2/static/sql-set.html
+ * For Sql Server : http://msdn.microsoft.com/en-us/library/ms190356.aspx
+ */
 class DATABASE_CONFIG {
 
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'websys',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'database_name',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
@@ -17,45 +65,10 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'alfonsotesttest',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
-	
-	
-	public $mongo = array(
-      'datasource' => 'Mongodb.MongodbSource',
-      'host' => 'localhost',
-      'database' => 'appsys',
-      'port' => 27017,
-      'prefix' => '',
-      'persistent' => 'true',
-      /* optional auth fields
-      'login' => 'mongo', 
-      'password' => 'awesomeness',
-      'replicaset' => array('host' => 'mongodb://hoge:hogehoge@localhost:27021,localhost:27022/blog', 
-                            'options' => array('replicaSet' => 'myRepl')
-                   ),
-      */
-  );
-  
-  public $test_mongo = array(
-		'datasource' => 'Mongodb.MongodbSource',
-		'host' => 'localhost',
-		'login' => '',
-		'password' => '',
-		'database' => 'appsys',
-		'port' => 27017,
-		'prefix' => '',
-    'persistent' => 'true',
-	);
-  
-  public $mongocake = array(
-      'datasource' => 'MongoCake.CakeMongoSource',
-      'server' => 'localhost', // Optional
-      'database' => 'appsys', // Database to use,
-      'port' => 27017,
-  );
 }
